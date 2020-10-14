100117081789e20e9394ac97af12eb4783941f5f
L <LZ_931491211@qq.com>
2020-10-14 22:52:23 +0800

    HW commit

---
+++ example1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+// 计算 
+{int x; 
+ int height;
+ float z;
+ z = 55.5;
+ height = 88;
+ x = 99 + 99;
+ int y = 99 * 99;  
+ printf("5 + 5 =%d\n", y); 
+ printf("5 + 5 =%d\n", 5 + 5);
+ printf("计算的结果=%d\n", x);
+ printf("计算的结果=%d\n", height);
+ printf("计算的结果=%d\n", z);
+ return 0;
+ } 
