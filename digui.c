17bd48c1a0ab40e1c9943c467e8c7c1102ff18c4
Hori-Ray <931491211@qq.com>
2020-12-15 08:22:03 +0800

    exercise

--- digui.c
+++ digui.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main (void){
     int a,n,k;
-	scanf("%d,%d,%d",&n,&a,&k);
-	if (n<2||n>100||a,k<=0||a,k>100)
+	scanf("%d %d %d",&n,&a,&k);
+	if (n<2||n>100||a<=0||k<=0||a>100||k>100)
 	printf("Wrong Number");
 	else
 	printf("%d",fac(n,a,k));
