a260641e228064592da556b82ac45f6c3e06faae
Hori-Ray <931491211@qq.com>
2021-01-16 19:26:26 +0800

    exercise

---
+++ erjinzhishuchu.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int num,len=0,i=0,a[30];
+	scanf("%d",&num);
+	do{
+		a[len] = num%2;
+		num = num/2;
+		len++;
+	}while(num);
+	for(i=0;i<len;i++)		printf("%d", a[len-i-1]);
+	printf("\n");
+	return 0;
+}
+
