ea25fb40b649d5a73e94aa123a0a76c7aa9b9feb
Hori-Ray <931491211@qq.com>
2021-01-16 19:27:30 +0800

    exercise

---
+++ zhidenizhuan.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+void inVert(int *s,int num); 
+int main() 
+{
+int i, n = 10, a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+inVert(a, n - 1);
+    for (i = 0; i< 10; i++)   printf("%4d", a[i]);
+printf("\n");
+    return 0;
+}
+void inVert(int *s,int num) {
+int *t, k;
+   t = s + num;
+   while (s < t) {
+      k = *s;
+      *s = *t;
+      *t = k;
+      s++;
+t--; 
+   }
+}
+
